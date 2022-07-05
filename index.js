41a2b17a47e555595891fb9ddcc58083f1ef143d
NwtonX <104652905+NwtonX@users.noreply.github.com>
2022-07-05 11:13:47 +0300

    undefined

---
+++ index.js
@@ -0,0 +1,61 @@
+// Queue list
+
+let queues = {};
+
+// Main
+
+function take_queue(name, callback)
+{
+	if (typeof callback != 'function')
+	{
+		// Invalid callback
+
+		return;
+	}
+
+	if (typeof name != 'string' || name.length < 1)
+	{
+		// Invalid queue name
+
+		return;
+	}
+
+	wait_queue(name, callback);
+}
+
+function wait_queue(name, callback)
+{
+	if (queues[name])
+	{
+		// Wait
+
+		setTimeout(wait_queue, 10, name, callback);
+	}
+	else
+	{
+		// Take
+
+		queues[name] = true;
+		callback();
+	}
+}
+
+function free_queue(name)
+{
+	if (typeof name != 'string' || name.length < 1)
+	{
+		// Invalid queue name
+
+		return;
+	}
+
+	delete queues[name];
+}
+
+// Exports
+
+module.exports =
+{
+	take: take_queue,
+	free: free_queue
+};
